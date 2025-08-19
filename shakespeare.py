744655ea4f006ad4c08de9f2712cd5318c9c456b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-08-19 10:51:18 +0200

    Remove punctuation from sonnets

--- shakespeare.py
+++ shakespeare.py
@@ -1,7 +1,9 @@
+import string
+
+
 def rude_words(sonnets, target_words):
-    sonnets_list = sonnets.split()
-    sonnets_list = [word.lower() for word in sonnets_list]
+    cleaned_sonnets = ''.join(ch for ch in sonnets if ch not in string.punctuation)
+    sonnets_list = [word.lower() for word in cleaned_sonnets.split()]
     for word in target_words:
-        print(word)
         is_used = sonnets_list.count(word)
     return f'{word}: {is_used}'
