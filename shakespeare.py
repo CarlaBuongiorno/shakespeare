b4faae4dd577a1dc03f150bb7b3e1e62e0757edf
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-08-19 10:21:22 +0200

    Add test for punctuation and fix tests target word input to be a list

--- shakespeare.py
+++ shakespeare.py
@@ -2,5 +2,6 @@ def rude_words(sonnets, target_words):
     sonnets_list = sonnets.split()
     sonnets_list = [word.lower() for word in sonnets_list]
     for word in target_words:
+        print(word)
         is_used = sonnets_list.count(word)
     return f'{word}: {is_used}'
