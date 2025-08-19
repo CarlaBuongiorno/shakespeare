b0c05b954ce48cf9a6ff720300c9b2d67baaa79b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-08-19 10:11:45 +0200

    Add test for capital letters

--- shakespeare.py
+++ shakespeare.py
@@ -1,5 +1,6 @@
 def rude_words(sonnets, target_words):
     sonnets_list = sonnets.split()
+    sonnets_list = [word.lower() for word in sonnets_list]
     for word in target_words:
-        used = sonnets_list.count(word)
-    return f'{word}: {used}'
+        is_used = sonnets_list.count(word)
+    return f'{word}: {is_used}'
--- test_shakespeare.py
+++ test_shakespeare.py
@@ -8,3 +8,8 @@ def test_rude_words_function_exists():
 def test_count_target_word():
     sonnets = 'This is a sonnet.'
     assert rude_words(sonnets, 'a') == f'a: 1'
+
+
+def test_target_word_has_capitals():
+    sonnets = 'This is A sonnet.'
+    assert rude_words(sonnets, 'a') == f'a: 1'
